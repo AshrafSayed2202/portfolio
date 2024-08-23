93178076ce24d9f2d20458378ed55e66645a2264
ashraf9932 <ashraf.neizk@gmail.com>
2024-08-23 23:15:22 +0300

    Clients Section Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
+import Clients from "@/components/Clients";
 import RecentProjects from "@/components/RecentProjects";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
                 <Hero />
                 <Grid />
                 <RecentProjects />
+                <Clients />
             </div>
         </main>
     );
