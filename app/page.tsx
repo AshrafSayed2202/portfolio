3e69e02b34ea4ad97c1c6120b90824b86146f947
ashraf9932 <ashraf.neizk@gmail.com>
2024-08-23 22:36:58 +0300

    Projects Section Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
+import RecentProjects from "@/components/RecentProjects";
 
 export default function Home() {
     return (
@@ -7,6 +8,7 @@ export default function Home() {
             <div className="max-w-7xl w-full">
                 <Hero />
                 <Grid />
+                <RecentProjects />
             </div>
         </main>
     );
