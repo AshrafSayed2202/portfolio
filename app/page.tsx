77affbe99385938211507f335a5aa4fd35e66702
ashraf9932 <ashraf.neizk@gmail.com>
2024-08-23 23:19:32 +0300

    Experience Section Done

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Hero from "@/components/Hero";
 import Grid from "@/components/Grid";
 import Clients from "@/components/Clients";
+import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 
 export default function Home() {
@@ -11,6 +12,7 @@ export default function Home() {
                 <Grid />
                 <RecentProjects />
                 <Clients />
+                <Experience />
             </div>
         </main>
     );
