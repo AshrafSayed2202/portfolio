0c1243d8788077915643ce71322a93e09fa7c587
ashraf9932 <ashraf.neizk@gmail.com>
2024-08-24 12:52:30 +0300

    update title + favicons

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ import { ThemeProvider } from "./provider";
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
-    title: "Adrian's Portfolio",
-    description: "Modern & Minimal JS Mastery Portfolio",
+    title: "Ashraf's Portfolio",
+    description: "MERN Stack & Next.JS Developer Portfolio.",
 };
 export default function RootLayout({
     children,
@@ -17,7 +17,10 @@ export default function RootLayout({
     return (
         <html lang="en" suppressHydrationWarning>
             <head>
-                <link rel="icon" href="/jsm-logo.png" sizes="any" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
             </head>
             <body className={inter.className}>
                 <ThemeProvider
